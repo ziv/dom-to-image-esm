d920109243cc874b6ac6bbcce71bd206a034beff
ziv-perry <ziv.perry@zoominfo.com>
2024-07-21 12:48:51 +0300

    build

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,5 @@
 const click = async () => {
-    const { domNodeToSvg } = await import('./imagify.js');
+    const { domNodeToSvg } = await import('./index.js');
     const svg = domNodeToSvg(document.getElementById('example') as HTMLElement);
     const { toImage } = await import('./utils.js');
     document.body.appendChild(toImage(svg));
--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,14 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
     build: {
         lib: {
-            // Could also be a dictionary or array of multiple entry points
             entry: {
-                'index' : 'src/imagify.ts',
+                'index' : 'src/index.ts',
                 'utils': 'src/utils.ts'
             },
             formats: ['es'],
         },
     },
     plugins: [
-        dts({ include: ['src'] })
+        dts({ include: ['src'], exclude: ['src/demo.ts'] })
     ]
 });

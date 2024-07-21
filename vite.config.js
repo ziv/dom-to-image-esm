f81d5a15c947c8c0c3cc8b58806fd5fe4617c4ee
ziv-perry <ziv.perry@zoominfo.com>
2024-07-21 20:38:05 +0300

    pub

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     build: {
         lib: {
             entry: {
-                'amy' : 'src/amy.ts',
-                'dracula': 'src/dracula.ts'
+                index: 'src/index.ts',
+                amy: 'src/amy.ts',
+                dracula: 'src/dracula.ts'
             },
             formats: ['es'],
         },
